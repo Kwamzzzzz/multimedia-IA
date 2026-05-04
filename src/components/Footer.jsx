90475c2c764817d8e482897f55aacc9bf917b8bc
Kwamzzzzz <francisamponsah2005@gmail.com>
2026-05-04 22:27:05 +0000

    Add files via upload
    src folder upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      Demo only. Do not enter real personal or financial information.
+    </footer>
+  );
+}
+
+export default Footer;
