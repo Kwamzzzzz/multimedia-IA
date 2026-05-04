db844198c1d4f7b1f4d9b01ab0282b454adbf07d
Kwamzzzzz <francisamponsah2005@gmail.com>
2026-05-04 23:08:59 +0000

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      Demo only. Do not enter real personal or financial information.
-    </footer>
-  );
-}
-
-export default Footer;
